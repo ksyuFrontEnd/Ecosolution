f0bc56dd1f2b536a12f1e0efe479c64a5bc83147
Oksana Baranik <ksyubaranik@gmail.com>
2024-07-02 10:49:01 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, World!r</h1>
+      <h2>Hi</h2>
+      <h3>Hi once again</h3>
+      <h4>Hello hello</h4>
+    </div>
+  );
+}
+
+export default App;
