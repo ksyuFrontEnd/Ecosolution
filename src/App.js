ec7d8b332a06c05b0029d57da32bf39cad51ca23
Oksana Baranik <ksyubaranik@gmail.com>
2024-07-02 12:34:49 +0200

    Added main and reset styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
+import "./styles/main.css"
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World!r</h1>
-      <h2>Hi</h2>
-      <h3>Hi once again</h3>
-      <h4>Hello hello</h4>
+      
     </div>
   );
 }
