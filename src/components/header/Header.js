9eba18d12ccef65e265c104ebcf16281a1a0f26b
Oksana Baranik <ksyubaranik@gmail.com>
2024-07-07 12:34:43 +0200

    Made header to change color while scrolling

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,12 +3,31 @@ import { ReactComponent as Logo } from '../../img/logo.svg';
 import { ReactComponent as ArrowDown } from '../../img/arrow-down.svg';
 import BurgerMenu from '../burger-menu/BurgerMenu';
 import { Link } from 'react-scroll';
+import { useState, useEffect } from 'react';
 
 function Header() {
+    const [isScrolled, setIsScrolled] = useState(false);
     const headerHeight = 112;
 
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollTop = window.scrollY;
+            if (scrollTop > headerHeight) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <header className="header">
+        <header className={`header ${isScrolled ? 'scrolled' : ''}`}>
             <div className="header-container container">
                 <a href="/"><Logo className="logo" width="269px" height="40px" fill="#173d33" /></a>
                 <div className="buttons__wrapper">
